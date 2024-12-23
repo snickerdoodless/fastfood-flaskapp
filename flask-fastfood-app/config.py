a98dfb5b12062f1f9836f2960d952a9eedc9adb0
Kheyral Sutan <166258400+snickerdoodless@users.noreply.github.com>
2024-12-23 12:27:05 +0700

    Update config.py

--- flask-fastfood-app/config.py
+++ flask-fastfood-app/config.py
@@ -2,7 +2,7 @@
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'feane'
-    MYSQL_PASSWORD = 'changeme'
+    MYSQL_PASSWORD = 'your_secure_password'
     MYSQL_DB = 'feane'
     SECRET_KEY = 'your_secret_key'
     SESSION_TYPE = 'filesystem'
